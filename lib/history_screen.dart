13dbe6e0a1d2c2373627f1d5264a38b578ec6418
Bruno Franco <138160017+BrunoFranco2003@users.noreply.github.com>
2025-06-28 15:23:39 -0300

    Update history_screen.dart

--- lib/history_screen.dart
+++ lib/history_screen.dart
@@ -1,1 +1,34 @@
-.
+// lib/history_screen.dart
+import 'package:flutter/material.dart';
+
+class HistoryScreen extends StatelessWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dummyHistory = [
+      'Busca: Av. Paulista, São Paulo',
+      'Busca: Rua Sete de Setembro, ES',
+      'Busca: Av. Atlântica, RJ',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Histórico de Buscas'),
+        // cor do AppBar herda o tema
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: dummyHistory.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: const Icon(Icons.history),
+              title: Text(dummyHistory[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
